7b781d3c59d82b70af666ea805d95eaa18750f79
krist0nian <krist0ian@yahoo.com>
2026-06-13 18:07:21 -0400

    Home Page completed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react'
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
     // BEM
+   <Router>
     <div className="App">
       <Header />
       <Home />
-    </div>
+    </div> 
+   </Router> 
+  
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -12,21 +12,41 @@ function Home() {
 
         <div className="home__row">
             <Product 
-            title='The lean Startup' 
+            title='The lean Startup: HOw Constant Innovation Creates Radically Successful Businesses. Paperback Edition' 
             price={29.99} 
             image="https://m.media-amazon.com/images/I/81-QB7nDh4L._SL1500_.jpg"
             rating={3} />
-            <Product />
+            <Product 
+            title ="Kenwood Mix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl" 
+            price={239.99}
+            rating={4}
+            image="https://www.buysbest.co.uk/wp-content/uploads/2022/12/image380056860.jpg" />
         </div>
 
         <div className="home__row">
-            <Product />
-            <Product />
-            <Product />
+            <Product 
+            title="Samsung Galaxy Fit Watch - SMR370NZKAXAR" 
+            price={199.99} 
+            image="https://image-us.samsung.com/SamsungUS/home/mobile/wearables/galaxy-fit/pdp/sm-r370nzsaxar/009-Gallery-Galaxy-Fit-Black.jpg?$product-details-jpg$"
+            rating={3}/>
+            <Product 
+            title="Amazon Echo (3rd Generation) Smart Speaker with Alexa, Charcoal Fabric" 
+            price={98.99} 
+            image="https://i.pinimg.com/736x/53/fd/07/53fd074455f3835b1e9594174855cc37.jpg"
+            rating={5} />
+            <Product 
+            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)" 
+            price={599.99} 
+            image="https://www.bhphotovideo.com/images/images2500x2500/apple_128gb_ipad_pro_wi_fi_1185490.jpg"
+            rating={4} />
         </div>
 
         <div className="home__row">
-            <Product />
+            <Product
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor" 
+            price={1094.99} 
+            image="https://m.media-amazon.com/images/I/6193kGlq9MS._AC_SL1500_.jpg"
+            rating={3}/>
         </div>
     </div>
 </div>
