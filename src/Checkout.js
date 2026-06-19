d72e539c90a02097967ea00699df114e289d8d90
krist0nian <krist0ian@yahoo.com>
2026-06-18 23:42:58 -0400

    Updated Add to Basket Counter

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import "./Checkout.css"
+import React from 'react';
+import "./Checkout.css";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
   return (
@@ -10,13 +11,15 @@ function Checkout() {
         src="https://predis.ai/resources/wp-content/uploads/2024/06/mceu_34882845131717072419-66587225ef085.png" alt="" />
         <div>
             <h2 className="checkout__title">
-                Your shopping basket
-            </h2>
+                Your shopping basket</h2>
+            {/* BasketItem */}
+            {/* BasketItem */}
+            {/* BasketItem */}
             
-        </div>
+            </div>
         </div>
         <div className="checkout__right">
-            <h2>The subtotal will go here</h2>
+            <Subtotal />
         </div>
     </div>
   )
